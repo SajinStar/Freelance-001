24a03218581f16623f329946a6512bc67d3da238
Joemn <Aruljoe37@gmail.com>
2021-06-30 21:36:16 +0530

    slider done

---
+++ app.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      document.getElementById("navbar_top").classList.add("fixed-top");
+      navbar_height = document.querySelector(".navbar").offsetHeight;
+      document.body.style.paddingTop = navbar_height + "px";
+    } else {
+      document.getElementById("navbar_top").classList.remove("fixed-top");
+      document.body.style.paddingTop = "0";
+    }
+  });
+});
+
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    450: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+});
