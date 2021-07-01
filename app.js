c633e257537a757db9bf67241285c4f683ceb86b
Joemn <Aruljoe37@gmail.com>
2021-07-01 22:14:40 +0530

    done

--- app.js
+++ app.js
@@ -48,3 +48,39 @@ const swiper = new Swiper(".swiper-container", {
     },
   },
 });
+
+const swiper2 = new Swiper(".swiper-container2", {
+  // Optional parameters
+  loop: true,
+  speed: 500,
+  // If we need pagination
+
+  slidesPerView: 3,
+  spaceBetween: 10,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    450: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
