fa11ff8e10ffc35ba4a65aea31b3077195ef59b6
Joemn <Aruljoe37@gmail.com>
2021-07-01 23:18:16 +0530

    fixed

--- app.js
+++ app.js
@@ -58,10 +58,7 @@ const swiper2 = new Swiper(".swiper-container2", {
   slidesPerView: 3,
   spaceBetween: 10,
   // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+
   breakpoints: {
     // when window width is >= 320px
     320: {
